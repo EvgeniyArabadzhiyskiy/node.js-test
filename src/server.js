f635a8a6d53c414323bd53587d1a72942058561b
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-23 12:17:05 +0300

    modified:   src/server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const {contactsRoutes} = require('./routes/contacts');
+// const {contactsRoutes} = require('./routes/contacts');
 
 const app = express()
 
-const PORT = process.env.PORT || 4000
+// const PORT = process.env.PORT || 4000
 
-app.use(express.json())
-app.use
+// app.use(express.json())
+
+// app.get("/", async (req, res)=> {
+//     res.json({message: "hello"})
+// });
+// app.use(contactsRoutes)
 
 
-app.listen(PORT)
+app.listen(4000)
