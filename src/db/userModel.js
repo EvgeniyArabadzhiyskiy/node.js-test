b1d882132457cd3b3e7ffc80f94ff9b149ba61fa
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-28 18:34:45 +0300

    add user model

---
+++ src/db/userModel.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  //   email: {
+  //     type: String,
+  //     required: true,
+  //   },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const User = model("User", userSchema);
+
+module.exports = {
+  User,
+};
