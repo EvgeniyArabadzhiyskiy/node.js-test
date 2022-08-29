2e795804e6a57abffb4714063df25bf2e0d97f3e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-29 23:42:23 +0300

    update post controllers

--- src/controllers/postControllers.js
+++ src/controllers/postControllers.js
@@ -10,14 +10,20 @@ const {
 } = require("../services/postService");
 
 const getPostController = async (req, res) => {
-  const posts = await getPost();
+  const { _id: userId } = req.user;
+  const { limit, page } = req.query;
+  const skipPost = (page - 1) * limit
+
+  const posts = await getPost({userId, limit, skipPost});
+  
 
   res.json({ posts, status: "success" });
 };
 
 const getPostByIdController = async (req, res) => {
-  const { id } = req.params;
-  const findPost = await getPostById(id);
+  const { id: postId } = req.params;
+  const { _id: userId } = req.user;
+  const findPost = await getPostById(postId, userId);
 
   if (!findPost) {
     return res
@@ -28,36 +34,38 @@ const getPostByIdController = async (req, res) => {
 };
 
 const addPostController = async (req, res) => {
-  const newPost = await addPost(req);
+  const { body } = req;
+  const { _id: userId } = req.user;
+  const newPost = await addPost(userId, body);
 
   res.status(201).json({ newPost, status: "success" });
 };
 
 const deletePostController = async (req, res) => {
-  const { id } = req.params;
-  const removedPost = await getPostById(id);
-  await deletePost(id);
+  const { id: postId } = req.params;
+  const { _id: userId } = req.user;
+  const removedPost = await deletePost(postId, userId);
 
   if (!removedPost) {
     return res
       .status(400)
-      .json({ message: `Post with  id=${req.params.id} Not Found` });
+      .json({ message: `Post with  id=${postId} Not Found` });
   }
 
   res.json({ removedPost, status: "success" });
 };
 
 const putPostController = async (req, res) => {
-  const { id } = req.params;
+  const { id: postId } = req.params;
   const { topics, text } = req.body;
+  const { _id: userId } = req.user;
 
-  await changePost(id, { topics, text });
-  const updatePost = await getPostById(id);
+  const updatePost = await changePost(postId, { topics, text }, userId);
 
   if (!updatePost) {
     return res
       .status(400)
-      .json({ message: `Post with  id=${req.params.id} Not Found` });
+      .json({ message: `Post with  id=${postId} Not Found` });
   }
 
   res.json({ updatePost, status: "success" });
