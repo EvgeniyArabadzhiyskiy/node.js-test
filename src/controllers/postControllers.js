d3efaf423742dfca195f4d8a2d1402c5a3d77ee3
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-31 21:58:54 +0300

    modified:   src/controllers/postControllers.js

--- src/controllers/postControllers.js
+++ src/controllers/postControllers.js
@@ -10,19 +10,19 @@ const {
 } = require("../services/postService");
 
 const getPostController = async (req, res) => {
-  const { _id: userId } = req.user;
+  const { _id: owner } = req.user;
   const { limit = 20, page = 1 } = req.query;
   const skipPost = (page - 1) * limit;
 
-  const posts = await getPost(userId, limit, skipPost);
+  const posts = await getPost(owner, limit, skipPost);
 
   res.json({ posts, status: "success" });
 };
 
 const getPostByIdController = async (req, res) => {
   const { id: postId } = req.params;
-  const { _id: userId } = req.user;
-  const findPost = await getPostById(postId, userId);
+  const { _id: owner } = req.user;
+  const findPost = await getPostById(postId, owner);
 
   if (!findPost) {
     return res
@@ -34,16 +34,16 @@ const getPostByIdController = async (req, res) => {
 
 const addPostController = async (req, res) => {
   const { body } = req;
-  const { _id: userId } = req.user;
-  const newPost = await addPost(userId, body);
+  const { _id: owner } = req.user;
+  const newPost = await addPost(owner, body);
 
   res.status(201).json({ newPost, status: "success" });
 };
 
 const deletePostController = async (req, res) => {
   const { id: postId } = req.params;
-  const { _id: userId } = req.user;
-  const removedPost = await deletePost(postId, userId);
+  const { _id: owner } = req.user;
+  const removedPost = await deletePost(postId, owner);
 
   if (!removedPost) {
     return res
@@ -57,9 +57,9 @@ const deletePostController = async (req, res) => {
 const putPostController = async (req, res) => {
   const { id: postId } = req.params;
   const { topics, text } = req.body;
-  const { _id: userId } = req.user;
+  const { _id: owner } = req.user;
 
-  const updatePost = await changePost(postId, { topics, text }, userId);
+  const updatePost = await changePost(postId, { topics, text }, owner);
 
   if (!updatePost) {
     return res
