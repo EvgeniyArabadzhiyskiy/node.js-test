7efdb9e87cf688666b2e3d965105e5c585efaf49
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-31 22:00:06 +0300

    update service

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -3,7 +3,9 @@ const express = require("express");
 const {
   registrationControleer,
   loginControleer,
+  logoutControleer,
 } = require("../controllers/authController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const controllerWrapper = require("../middlewares/controllerWrapper");
 
@@ -11,5 +13,6 @@ const router = express.Router();
 
 router.post("/registration", controllerWrapper(registrationControleer));
 router.post("/login", controllerWrapper(loginControleer));
+router.post("/logout", authMiddleware, controllerWrapper(logoutControleer));
 
 module.exports = { authRouter: router };
