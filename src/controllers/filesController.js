9e084bfe7de13cdc55661702521058acc4b0fea0
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-09-03 11:52:20 +0300

    update files controler

--- src/controllers/filesController.js
+++ src/controllers/filesController.js
@@ -1,5 +1,32 @@
-const filesController = async (req, res) => {
-  res.json({ status: "success" });
+const fs = require("fs/promises");
+const path = require("path");
+
+const postDir = path.join(__dirname, "../../public", "post");
+
+const filesController = async (req, res, next) => {
+
+  if (!req.file) {
+    next();
+    return req.body
+  }
+
+  const { path: oldPath, filename } = req.file;
+  const newPath = path.join(postDir, filename);
+
+  const imagePath = path.join("post", filename);
+
+  try {
+    await fs.rename(oldPath, newPath);
+
+    req.body = {
+      ...req.body,
+      image: imagePath,
+    };
+  } catch (error) {
+    await fs.unlink(oldPath);
+  }
+
+  next();
 };
 
 module.exports = {
