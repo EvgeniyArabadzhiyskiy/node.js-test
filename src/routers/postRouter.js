b59230ee176b13efaafff1f4fcbd2dd54ee06c87
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-09-03 11:57:06 +0300

    update post router

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -17,13 +17,18 @@ const {
 const controllerWrapper = require("../middlewares/controllerWrapper");
 const authMiddleware = require("../middlewares/authMiddleware");
 
+
+const uploadMiddleware = require("../middlewares/uploadMiddleware");
+const { filesController } = require("../controllers/filesController");
+
 const router = express.Router();
 
 router.use(authMiddleware)
 
 router.get("/", controllerWrapper(getPostController));
 router.get("/:id", controllerWrapper(getPostByIdController));
-router.post("/",  controllerWrapper(addPostController));
+router.post("/", uploadMiddleware.single("avatar"), controllerWrapper(filesController), controllerWrapper(addPostController));
+router.post("/upload", uploadMiddleware.single("avatar"), controllerWrapper(filesController));
 router.put("/:id", putPostValidation, controllerWrapper(putPostController));
 router.delete("/:id", controllerWrapper(deletePostController));
 
