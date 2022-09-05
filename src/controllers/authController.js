e6cdc105b9d2f59d216c55541eeac22f07c1f0bb
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-09-05 18:30:41 +0300

    update controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,17 @@
+const path = require("path");
+const fs = require("fs/promises");
+const shortid = require("shortid");
+
 const { User } = require("../db/userModel");
-const { registration, login, logout } = require("../services/authService");
+const { registration, login, logout, repeatVerify, verifyEmail } = require("../services/authService");
+
+const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 
 const registrationControleer = async (req, res) => {
   const { name, email, password } = req.body;
+  const verificationToken = shortid()
 
-  const user = await registration(name, email, password);
+  const user = await registration(name, email, password, verificationToken);
 
   res.json({ user, status: "success",  });
 };
@@ -28,14 +35,45 @@ const logoutControleer = async (req, res) => {
 const currentUserControleer = async (req, res) => {
   const { _id } = req.user;
 
-  const { name, createAt } = await User.findById(_id);
+  const { name, createAt, avatarUrl } = await User.findById(_id);
 
-  res.json({ status: "success", name, createAt });
+  res.json({ status: "success", name, createAt, avatarUrl });
 };
 
-const verifyEmailControleer = async (req, res) => {
+
+const updateAvatarController = async (req, res) => {
+  const {_id} = req.user
+  const { path: originalPath, filename } = req.file;
+  const endPath = path.join(avatarDir, filename); 
+
+  const avatarUrl = path.join( "avatars", filename);
+
+  try {
+    await fs.rename(originalPath, endPath);
+    await User.findByIdAndUpdate(_id, { avatarUrl })
+
+    res.json({avatarUrl})
+  } catch (error) {
+    await fs.unlink(originalPath)
+  }
+};
+
+const verifyEmailControleer = async (req, resp) => {
   const {verificationToken} = req.params
+
+  const ddd = await verifyEmail(verificationToken)
+  console.log("verifyEmailControleer ~ ddd", ddd);
+
+  resp.json({ message: `Verification successful`});
   
+};
+
+const repeatVerifyControleer = async (req, res) => {
+  const { email } = req.body
+
+  await repeatVerify(email)
+
+  res.json({ message: `Verification message send to email ${email}`});
 };
 
 module.exports = {
@@ -43,5 +81,7 @@ module.exports = {
   loginControleer,
   logoutControleer,
   currentUserControleer,
+  updateAvatarController,
   verifyEmailControleer,
+  repeatVerifyControleer
 };
