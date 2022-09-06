565c495db28324db4721b52d1f1e0e0f26e3054e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-09-06 20:07:25 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,41 +1,16 @@
 const express = require("express");
-const { connectionMongo } = require("./src/db/connections");
-const { authRouter } = require("./src/routers/authRouter");
-const { postRouter } = require("./src/routers/postRouter");
-require("dotenv").config();
-
-const PORT = process.env.PORT;
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
-app.use(express.json());
 app.use(express.static("public"));
-app.use("/api/auth", authRouter);
-app.use("/api/posts", postRouter);
 
-app.use((req, response) => {
-  response.send("ERROR");
-});
-
-app.use((err, req, res, next) => {
-  // const { status = 500, message = "Server error" } = err;
-  res.status(500).json({ message: err.message });
-});
-
-const start = async () => {
-  await connectionMongo();
-
-  // ========== server =================
-  app.listen(PORT, (err) => {
-    if (err) {
-      console.error(err);
-    }
-    console.log(`Server run on port ${PORT}`);
+io.on("connection", (socket) => {
+  socket.on("MESSAGE_FROM_CLIEN", ({ message, userName }) => {
+    io.emit("SERVER_MESSAGE_TO_ALL", `${userName}: ${message}`);
   });
-};
+});
 
-start();
-
-// npx nodemon server.js
-
-
-
+server.listen(8085, () => {
+  console.log("server run on PORT 8085");
+});
