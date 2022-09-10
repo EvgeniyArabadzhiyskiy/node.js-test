c5a6744d2e0806b6f81c47c24c500972c93e8b8f
EvgeniyArabadzhiyskiy <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-09-10 23:20:08 +0300

    update files router

--- src/routers/filesRouter.js
+++ src/routers/filesRouter.js
@@ -1,7 +1,5 @@
 const express = require("express");
-// const multer = require("multer");
 const path = require("path");
-// const shortid = require("shortid");
 
 const { filesController } = require("../controllers/filesController");
 const controllerWrapper = require("../middlewares/controllerWrapper");
@@ -9,7 +7,7 @@ const uploadMiddleware = require("../middlewares/uploadMiddleware");
 
 const router = express.Router();
 
-const FILE_DIR = path.join(__dirname, "../../tmp/prod");
+const FILE_DIR = path.join(__dirname, "../../tmp");
 
 // const storage = multer.diskStorage({
 //   destination: function (req, file, cb) {
